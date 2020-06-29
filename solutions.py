cc8ab1cd89ced1ff3635f75dc39dc5a02afce88c
L30n95 <43123269+L30n95@users.noreply.github.com>
2020-06-30 00:03:54 +0100

    1 x problem
    1 x problem

--- solutions.py
+++ solutions.py
@@ -6,7 +6,7 @@ import os
 import random
 import re
 import sys
-'''
+"""
 Task
 Given an integer, , perform the following conditional actions:
 
@@ -15,7 +15,7 @@ If n is even and in the inclusive range of  to 2,5 print Not Weird
 If n is even and in the inclusive range of  to 6,20 print Weird
 If n is even and greater than 20, print Not Weird
 
-'''
+
 
 
 if __name__ == '__main__':
@@ -30,3 +30,73 @@ if __name__ == '__main__':
             print("Not Weird")
 
 
+
+
+
+Task
+Read two integers from STDIN and print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    
+    
+Task
+Read two integers and print two lines. The first line should contain integer division,  // . The second line should contain float division,  / .
+
+You don't need to perform any rounding or formatting operations.
+
+    
+
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(int(a / b))
+    print(float(a / b)) 
+    
+Task
+Read an integer . For all non-negative integers i < N , print i^2.  
+     
+    
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range (0,n):
+        print(i**2)
+
+
+"""
+# Operators test on HackerRank
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    # Complete the solve function below.
+    def solve(meal_cost, tip_percent, tax_percent):
+        answer = meal_cost + (meal_cost * (tip_percent / 100)) + (meal_cost * (tax_percent / 100))
+
+        print(round(answer))
+        return
+
+meal_cost = float(input())
+
+tip_percent = int(input())
+
+tax_percent = int(input())
+
+solve(meal_cost, tip_percent, tax_percent)
